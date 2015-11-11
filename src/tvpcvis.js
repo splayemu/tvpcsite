d96e6073e60e14e4992f9e4b3776d85d3d48a981
Spencer Apple <splayemu@gmail.com>
2015-11-11 13:29:26 -0400

    Converted scales to use d3 scales.

--- src/tvpcvis.js
+++ src/tvpcvis.js
@@ -1,35 +1,33 @@
 soundcloudClientID = '3bfa6c4e7c64be439b555750443fcab7';
 tvpcSoundcloudID = '5035743';
 
-SC.initialize({
-    client_id: soundcloudClientID
-});
+//SC.initialize({
+//    client_id: soundcloudClientID
+//});
 // use the soundcloud api to get TVPC song stats
 console.log('Everything seems ship shape');
 
-SC.get('/users/' + tvpcSoundcloudID + '/tracks/')
-  .then(function(tracks){
-      console.log(tracks);
-      drawSongs(tracks);})
-  .catch(function(error){
-      alert('Error: ' + error.message);
-});
+//SC.get('/users/' + tvpcSoundcloudID + '/tracks/')
+//  .then(function(tracks){
+//      console.log(tracks);
+//      drawSongs(tracks);})
+//  .catch(function(error){
+//      alert('Error: ' + error.message);
+//});
 
 var popularity = function popularityF (track) {
     return track.favoritings_count;
 }
 
-function Scale(minLoc, maxLoc) {
+function Scale() {
     // force new to be called
     if (!(this instanceof Scale)) {
         return new Scale(minLoc, maxLoc);
     }
 
-    this.minLoc = minLoc;
-    this.maxLoc = maxLoc;
+    console.log('Scale initialized: ');
     this.minValue = null;
     this.maxValue = null;
-    this.buffer = 0;
 }
 
 Scale.prototype.addValue = function (value) {
@@ -41,48 +39,53 @@ Scale.prototype.addValue = function (value) {
         this.maxValue = value;
 }
 
-Scale.prototype.toScale = function (value) {
-    if(this.minValue === null)
-        return null;
-
-    // rate = plot / value
-    var valueRate = Math.max(1, this.maxValue - this.minValue);
-    var rate = (this.maxLoc - this.minLoc - 2 * this.buffer) / valueRate;
-
-    return this.minLoc + this.buffer + rate * (value - this.minValue);
+Scale.prototype.getMinMax = function () {
+    console.log('Scale::getMinMax ', this.minValue, ' ', this.maxValue);
+    return [this.minValue, this.maxValue];
 }
 
 var calculateScales = function calculateScalesF (tracks) {
-    // need to put everything into scale
-    // x axis is date released
-    // y axis is bpm? length?
+    // minCircleSize should be calculated based on the size of the stroke-width
+    // maxCircleSize should be calculated based on the size of the viewbox
+    // buffer is maxCircleSize + stroke-width
+    var minCircleSize = 4,
+        maxCircleSize = 20,
+        buffer = maxCircleSize + 4;
 
     var vis = d3.select('#vis')
-    var maxCircleSize = 0;
 
-    var xScale = new Scale(0, 800);
-    var yScale = new Scale(0, 600);
+    var rect = vis.node().getBoundingClientRect();
+    console.log(rect);
+
+    var xValues = new Scale();
+    var yValues = new Scale();
+    var rValues = new Scale();
 
     for (var i in tracks) {
         if (tracks.hasOwnProperty(i)) {
             console.log('adding', i, ":", tracks[i]);
-            yScale.addValue(tracks[i].duration);
 
-            var birthSeconds = Date.parse(tracks[i].created_at)
-            xScale.addValue(birthSeconds);
-
-            // radius
-            if (popularity(tracks[i]) > maxCircleSize)
-                maxCircleSize = popularity(tracks[i]);
+            xValues.addValue(tracks[i].created_at);
+            yValues.addValue(tracks[i].duration);
+            rValues.addValue(popularity(tracks[i]));
         }
     }
-    xScale.buffer = maxCircleSize + 1;
-    yScale.buffer = maxCircleSize + 1;
 
-    return [xScale, yScale];
+    var xScale = d3.time.scale()
+        .domain(xValues.getMinMax())
+        .range([buffer, rect['width'] - buffer]);
+
+    var yScale = d3.scale.linear()
+        .domain(yValues.getMinMax())
+        .range([buffer, rect['height'] - buffer]);
+
+    var rScale = d3.scale.log()
+        .domain(rValues.getMinMax())
+        .range([minCircleSize, maxCircleSize]);
+
+    return [xScale, yScale, rScale];
 }
 
-// use d3 to draw circles based on the number of likes
 function drawSongs(tracks) {
 
     var scales = calculateScales(tracks);
@@ -90,9 +93,40 @@ function drawSongs(tracks) {
     var circle = d3.select("#vis").selectAll("circle")
         .data(tracks)
         .enter().append("circle")
-        .attr("cx", function(d) { return scales[0].toScale(Date.parse(d.created_at)); })
-        .attr("cy", function(d) { return scales[1].toScale(d.duration); })
-        .attr("r", popularity);
+        .attr("cx", function(d) { return scales[0](d.created_at); })
+        .attr("cy", function(d) { return scales[1](d.duration); })
+        .attr("r", function(d) { return scales[2](popularity(d)); })
 }
 
-//document.addEventListener("DOMContentLoaded", main);
+function testVis() {
+    var fakeTracks = [];
+
+    fakeTracks[0] = {
+        'duration': 5000,
+        'created_at': new Date(300000),
+        'favoritings_count': 5
+    };
+
+    fakeTracks[1] = {
+        'duration': 3000,
+        'created_at': new Date(600000),
+        'favoritings_count': 100
+    };
+
+    fakeTracks[2] = {
+        'duration': 1000,
+        'created_at': new Date(900000),
+        'favoritings_count': 5000
+    };
+
+    fakeTracks[3] = {
+        'duration': 2560,
+        'created_at': new Date(105000),
+        'favoritings_count': 20000
+    };
+
+    console.log(fakeTracks);
+    drawSongs(fakeTracks);
+}
+
+document.addEventListener("DOMContentLoaded", testVis);
